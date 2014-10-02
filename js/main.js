a807a777c90f1def2423d614d3c1f160323e9c03
Chris Mahlke <cmahlke@esri.com>
2014-10-02 15:10:08 -0700

    tmp

--- js/main.js
+++ js/main.js
@@ -19,17 +19,23 @@ require([
 	"dojo/store/Memory",
 	"dojo/store/Observable",
 	"esri/map",
-	"esri/arcgis/utils",
-	"esri/request",
+	"esri/layers/ArcGISDynamicMapServiceLayer",
+	"esri/tasks/IdentifyTask",
+	"esri/tasks/IdentifyParameters",
 	"dojo/parser",
 	"dojo/ready"
-], function (BorderContainer, ContentPane, registry, array, declare, lang, win, Deferred, dom, domAttr, domConstruct, domStyle, json, number, on, all, query, Memory, Observable, Map, arcgisUtils, esriRequest, parser, ready) {
+], function (BorderContainer, ContentPane, registry, array, declare, lang, win, Deferred, dom, domAttr, domConstruct, domStyle, json, number, on, all, query, Memory, Observable, Map, ArcGISDynamicMapServiceLayer, IdentifyTask, IdentifyParameters, parser, ready) {
 
 	parser.parse();
 
 	ready(function () {
 
-		var map;
+		var map,
+				countyLayer,
+				countyLayerUrl = "http://server.arcgisonline.com/arcgis/rest/services/Demographics/USA_Median_Household_Income/MapServer",
+				identifyUrl = "http://server.arcgisonline.com/arcgis/rest/services/Demographics/USA_Median_Household_Income/MapServer",
+				identifyTask,
+				identifyParams;
 
 		init();
 
@@ -39,6 +45,29 @@ require([
 				center:[-96.767578, 39.655399],
 				zoom:4
 			});
+
+			countyLayer = new ArcGISDynamicMapServiceLayer(countyLayerUrl, {
+				useMapImage:true
+			});
+			map.addLayer(countyLayer);
+			map.on("click", doIdentify);
+
+			identifyTask = new IdentifyTask(identifyUrl);
+			identifyParams = new IdentifyParameters();
+			identifyParams.tolerance = 3;
+			identifyParams.returnGeometry = true;
+			identifyParams.layerIds = [0, 2];
+			identifyParams.layerOption = IdentifyParameters.LAYER_OPTION_VISIBLE;
+			identifyParams.width = map.width;
+			identifyParams.height = map.height;
+
+			function doIdentify(event) {
+				identifyParams.geometry = event.mapPoint;
+				identifyParams.mapExtent = map.extent;
+				identifyTask.execute(identifyParams, function (results) {
+					console.log(results);
+				});
+			}
 		}
 	});
 });
