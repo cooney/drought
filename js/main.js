bac0efb7d5b9c1f5fa26ebe23d6e90e5c12cdbd0
Chris Mahlke <cmahlke@esri.com>
2014-10-08 19:25:47 -0700

    tmp

--- js/main.js
+++ js/main.js
@@ -73,8 +73,10 @@ require([
 				chart = c3.generate({
 					data:{
 						columns:[
-							['data1', 30, 200, 100, 400, 150, 250],
-							['data2', 50, 20, 10, 40, 15, 25]
+							['data1', 0, 100],
+							['data2', 0, 100],
+							['data3', 0, 100],
+							['data4', 0, 100]
 						],
 						types:{
 							data1:'area-spline',
