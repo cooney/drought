7a9402dc025e6d6d7ef8c48faea119fa5ab86cfc
Chris Mahlke <cmahlke@esri.com>
2014-10-10 15:59:14 -0700

    tmp

--- js/main.js
+++ js/main.js
@@ -71,49 +71,46 @@ require([
 			identifyParams.height = map.height;
 
 			function mapLoadedHandler() {
-				/*chart = c3.generate({
-				 data:{
-				 x: 'x',
-				 columns:[
-				 ['data0', 0, 0],
-				 ['data1', 0, 0],
-				 ['data2', 0, 0],
-				 ['data3', 0, 0],
-				 ['data4', 0, 0]
-				 ],
-				 types:{
-				 data0:'area-spline',
-				 data1:'area-spline',
-				 data2:'area-spline',
-				 data3:'area-spline',
-				 data4:'area-spline'
-				 // 'line', 'spline', 'step', 'area', 'area-step' are also available to stack
-				 },
-				 groups:[
-				 ['data0', 'data1', 'data2', 'data3', 'data4']
-				 ],
-				 onclick:function (d, element) {
-				 console.log("onclick", d, element);
-				 },
-				 onmouseover:function (d) {
-				 console.log("onmouseover", d);
-				 },
-				 onmouseout:function (d) {
-				 console.log("onmouseout", d);
-				 }
-				 },
-				 legend:{
-				 show:false
-				 },
-				 axis:{
-				 x:{
-				 type:"timeseries",
-				 tick:{
-				 count:10
-				 }
-				 }
-				 }
-				 });*/
+				/*var pieChart = c3.generate({
+					data:{
+						columns:[
+							['data1', 30],
+							['data2', 120]
+						],
+						type:'donut',
+						onclick:function (d, i) {
+							console.log("onclick", d, i);
+						},
+						onmouseover:function (d, i) {
+							console.log("onmouseover", d, i);
+						},
+						onmouseout:function (d, i) {
+							console.log("onmouseout", d, i);
+						}
+					},
+					donut:{
+						title:""
+					}
+				});
+
+				setTimeout(function () {
+					pieChart.load({
+						columns:[
+							["setosa", 0.2, 0.2, 0.2, 0.2, 0.2, 0.4, 0.3, 0.2, 0.2, 0.1, 0.2, 0.2, 0.1, 0.1, 0.2, 0.4, 0.4, 0.3, 0.3, 0.3, 0.2, 0.4, 0.2, 0.5, 0.2, 0.2, 0.4, 0.2, 0.2, 0.2, 0.2, 0.4, 0.1, 0.2, 0.2, 0.2, 0.2, 0.1, 0.2, 0.2, 0.3, 0.3, 0.2, 0.6, 0.4, 0.3, 0.2, 0.2, 0.2, 0.2],
+							["versicolor", 1.4, 1.5, 1.5, 1.3, 1.5, 1.3, 1.6, 1.0, 1.3, 1.4, 1.0, 1.5, 1.0, 1.4, 1.3, 1.4, 1.5, 1.0, 1.5, 1.1, 1.8, 1.3, 1.5, 1.2, 1.3, 1.4, 1.4, 1.7, 1.5, 1.0, 1.1, 1.0, 1.2, 1.6, 1.5, 1.6, 1.5, 1.3, 1.3, 1.3, 1.2, 1.4, 1.2, 1.0, 1.3, 1.2, 1.3, 1.3, 1.1, 1.3],
+							["virginica", 2.5, 1.9, 2.1, 1.8, 2.2, 2.1, 1.7, 1.8, 1.8, 2.5, 2.0, 1.9, 2.1, 2.0, 2.4, 2.3, 1.8, 2.2, 2.3, 1.5, 2.3, 2.0, 2.0, 1.8, 2.1, 1.8, 1.8, 1.8, 2.1, 1.6, 1.9, 2.0, 2.2, 1.5, 1.4, 2.3, 2.4, 1.8, 1.8, 2.1, 2.4, 2.3, 1.9, 2.3, 2.5, 2.3, 1.9, 2.0, 2.3, 1.8],
+						]
+					});
+				}, 1500);
+
+				setTimeout(function () {
+					pieChart.unload({
+						ids:'data1'
+					});
+					pieChart.unload({
+						ids:'data2'
+					});
+				}, 2500);*/
 			}
 
 			function doIdentify(event) {
@@ -185,11 +182,11 @@ require([
 											}
 										},
 										names:{
-											D0:"",
-											D1:"",
-											D2:"",
-											D3:"",
-											D4:"severe"
+											D0:"Dry",
+											D1:"Moderate",
+											D2:"Severe",
+											D3:"Extreme",
+											D4:"Exceptional"
 										}
 									},
 									size:{
