85997a19aa80a0ddcef9cd770df3e344854a7d53
Chris Mahlke <cmahlke@esri.com>
2014-10-30 11:16:42 -0700

    tmp

--- js/main.js
+++ js/main.js
@@ -62,7 +62,110 @@ require([
 				scrubberNode,
 				noResultsNode,
 				columnData = [],
-				currentData;
+				currentData,
+				//customLods = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
+				customLods = [
+					{
+						"level":0,
+						"resolution":156543.03392800014,
+						"scale":5.91657527591555E8
+					},
+					{
+						"level":1,
+						"resolution":78271.51696399994,
+						"scale":2.95828763795777E8
+					},
+					{
+						"level":2,
+						"resolution":39135.75848200009,
+						"scale":1.47914381897889E8
+					},
+					{
+						"level":3,
+						"resolution":19567.87924099992,
+						"scale":7.3957190948944E7
+					},
+					{
+						"level":4,
+						"resolution":9783.93962049996,
+						"scale":3.6978595474472E7
+					},
+					{
+						"level":5,
+						"resolution":4891.96981024998,
+						"scale":1.8489297737236E7
+					},
+					{
+						"level":6,
+						"resolution":2445.98490512499,
+						"scale":9244648.868618
+					},
+					{
+						"level":7,
+						"resolution":1222.992452562495,
+						"scale":4622324.434309
+					},
+					{
+						"level":8,
+						"resolution":611.4962262813797,
+						"scale":2311162.217155
+					},
+					{
+						"level":9,
+						"resolution":305.74811314055756,
+						"scale":1155581.108577
+					}/*,
+					{
+						"level":10,
+						"resolution":152.87405657041106,
+						"scale":577790.554289
+					},
+					{
+						"level":11,
+						"resolution":76.4370282850732,
+						"scale":288895.277144
+					},
+					{
+						"level":12,
+						"resolution":38.2185141425366,
+						"scale":144447.638572
+					},
+					{
+						"level":13,
+						"resolution":19.1092570712683,
+						"scale":72223.819286
+					},
+					{
+						"level":14,
+						"resolution":9.55462853563415,
+						"scale":36111.909643
+					},
+					{
+						"level":15,
+						"resolution":4.77731426794937,
+						"scale":18055.954822
+					},
+					{
+						"level":16,
+						"resolution":2.38865713397468,
+						"scale":9027.977411
+					},
+					{
+						"level":17,
+						"resolution":1.1943285668550503,
+						"scale":4513.988705
+					},
+					{
+						"level":18,
+						"resolution":0.5971642835598172,
+						"scale":2256.994353
+					},
+					{
+						"level":19,
+						"resolution":0.29858214164761665,
+						"scale":1128.497176
+					}*/
+				];
 
 		init();
 
@@ -120,7 +223,8 @@ require([
 
 			var createMapOptions = {
 				mapOptions:{
-					slider:true
+					slider:true,
+					lods:customLods
 				},
 				usePopupManager:true
 			};
@@ -141,6 +245,7 @@ require([
 				map.addLayer(countyLayer);
 
 				map.on("click", mapClickHandler);
+				//map.on("extent-change", mapExtentChangeHandler);
 				map.on("layer-add", layerAddHandler);
 
 				loadGeococder(map);
@@ -157,6 +262,10 @@ require([
 				console.log("Error: ", error.code, " Message: ", error.message);
 				deferred.cancel();
 			});
+
+			/*function mapExtentChangeHandler(extent) {
+				console.log(extent);
+			}*/
 
 			function layerAddHandler(layer) {
 				$("#draggable2").draggable({
@@ -199,19 +308,20 @@ require([
 
 						$("#no-results").fadeOut("slow");
 						domStyle.set(loadingIndicatorNode, "display", "block");
-						domConstruct.create("div", {
-							id: "maskingDiv",
-							style: {
-								"width" : "100%",
-								"height": "100%",
-								"top": 0,
-								"left": 0,
-								"background-color" : "red",
-								"position": "absolute",
-								"opacity" : 0.0,
-								"z-index" : 100
+						$("#test").css("display", "block");
+						/*domConstruct.create("div", {
+							id:"maskingDiv",
+							style:{
+								"width":"100%",
+								"height":"100%",
+								"top":0,
+								"left":0,
+								"background-color":"red",
+								"position":"absolute",
+								"opacity":0.0,
+								"z-index":100
 							}
-						}, dom.byId("centerPane"));
+						}, dom.byId("centerPane"));*/
 
 						var query = new Query();
 						query.returnGeometry = false;
@@ -408,10 +518,12 @@ require([
 
 							dom.byId("countyName").innerHTML = selectedCountyName + ", " + selectedState;
 						}).then(function (response) {
-							domStyle.set(loadingIndicatorNode, "display", "none");
-							domStyle.set(scrubberNode, "display", "block");
-							domConstruct.destroy("maskingDiv");
-						});
+									domStyle.set(loadingIndicatorNode, "display", "none");
+									domStyle.set(scrubberNode, "display", "block");
+									setTimeout(function() {
+										$("#test").css("display", "none");
+									}, 1000);
+								});
 					} else {
 						$("#no-results").fadeIn("slow");
 						dom.byId("countyName").innerHTML = "";
